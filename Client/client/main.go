67bb3952580e2f0123f45fcfcd1a5acd414180e4
Frank <frank.ankch17@gmail.com>
2023-08-07 13:21:18 +0800

    wait all account get locks then commit in client

--- Client/client/main.go
+++ Client/client/main.go
@@ -4,27 +4,76 @@ import (
 	pb "Client"
 	"context"
 	"fmt"
+	"math/rand"
+	"sync"
 	"time"
 
 	"google.golang.org/grpc"
 )
 
-func send_test(serverIP string) {
-	conn, err := grpc.Dial(serverIP, grpc.WithInsecure())
+type accountData struct {
+	serverIP  string
+	accountID uint
+}
+
+func getLock(amount int64, transactionId uint64, data accountData, wg *sync.WaitGroup, flag *bool, c pb.TwoPhaseCommitServerClient, ctx context.Context) {
+	defer wg.Done()
+
+	r, err := c.GetLock(ctx, &pb.Payment{Id: transactionId, From: uint64(data.accountID), Amount: amount})
 	if err != nil {
 		fmt.Println(err)
 	}
-	defer conn.Close()
-	c := pb.NewTwoPhaseCommitServerClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-	r, err := c.GetLock(ctx, &pb.Payment{Id: 0, From: 1, Amount: 1})
+	fmt.Println(data.serverIP, r.GetSuccessful())
+	*flag = r.GetSuccessful()
+}
+
+func send_transaction(sender accountData, receiver accountData, amount int64) {
+	// setup sender grpc connection
+	conn1, err := grpc.Dial(sender.serverIP, grpc.WithInsecure())
 	if err != nil {
 		fmt.Println(err)
 	}
-	c.Commit(ctx, &pb.PaymentID{Id: 0})
-	fmt.Println(serverIP, r.GetSuccessful())
+	defer conn1.Close()
+	c1 := pb.NewTwoPhaseCommitServerClient(conn1)
+	ctx1, cancel1 := context.WithTimeout(context.Background(), time.Second)
+	defer cancel1()
+	//setup receiver grpc connection
+	conn2, err := grpc.Dial(receiver.serverIP, grpc.WithInsecure())
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer conn2.Close()
+	c2 := pb.NewTwoPhaseCommitServerClient(conn1)
+	ctx2, cancel2 := context.WithTimeout(context.Background(), time.Second)
+	defer cancel2()
+
+	var senderTransactionId, receiverTransactionId uint64
+	senderTransactionId = rand.Uint64()
+	if sender.serverIP == receiver.serverIP {
+		fmt.Println("same server")
+		receiverTransactionId = rand.Uint64()
+	} else {
+		receiverTransactionId = senderTransactionId
+	}
+	fmt.Println(senderTransactionId, receiverTransactionId)
+	fmt.Print(123)
+	flag := true
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	go getLock(-amount, senderTransactionId, sender, &wg, &flag, c1, ctx1)
+	go getLock(amount, receiverTransactionId, receiver, &wg, &flag, c2, ctx2)
+	wg.Wait()
+	if flag {
+		a, _ := c1.Commit(ctx1, &pb.PaymentID{Id: senderTransactionId})
+		fmt.Println(a)
+		b, _ := c2.Commit(ctx2, &pb.PaymentID{Id: receiverTransactionId})
+		fmt.Println(b)
+	} else {
+		c1.Abort(ctx1, &pb.PaymentID{Id: senderTransactionId})
+		c2.Abort(ctx2, &pb.PaymentID{Id: receiverTransactionId})
+	}
+
 }
 func main() {
-	send_test("localhost:50051")
+	send_transaction(accountData{"localhost:50051", 1}, accountData{"localhost:50051", 2}, 1)
 }
